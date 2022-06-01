8691d316e8d3136a0331af20e9809c5e8deae97b
iremacildi <iremacildi@gmail.com>
2022-06-01 23:36:42 +0300

    dog - walk animation

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -2,9 +2,15 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    public Transform camera;
     public Animator anim;
-    public float speed;
-    public float rotationSpeed;
+    public float speed = 3;
+    public float rotationSpeed = 720;
+    Quaternion rotation;
+    public float cameraHeight = 2;
+    public float cameraDistance = 4;
+    int multiplier = 1;
+    bool keyPressed = false;
 
     void Start()
     {
@@ -13,8 +19,13 @@ public class PlayerMove : MonoBehaviour
 
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        if(Input.GetKey(KeyCode.LeftShift) && !keyPressed) {
+            keyPressed = true;
+            multiplier = multiplier * -1;
+        }
+
+        float horizontalInput = Input.GetAxis("Horizontal") * multiplier;
+        float verticalInput = Input.GetAxis("Vertical") * multiplier;
 
         anim.SetFloat("vertical",verticalInput);
         anim.SetFloat("horizontal",horizontalInput);
@@ -26,9 +37,21 @@ public class PlayerMove : MonoBehaviour
 
         if(movementDirection != Vector3.zero)
         {
-            //transform.forward = movementDirection;
+            transform.forward = movementDirection;
             Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+            rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+            transform.rotation = rotation;            
+        }     
+
+        //camera follow
+        Vector3 cameraMovementDirection = new Vector3(0, 0, -(verticalInput + (multiplier * cameraDistance)));
+        cameraMovementDirection.Normalize();
+        camera.transform.forward = cameraMovementDirection; 
+        camera.transform.position = transform.position + new Vector3(0, cameraHeight, multiplier * cameraDistance);
+
+        if(keyPressed && !Input.GetKey(KeyCode.LeftShift))
+        {
+            keyPressed = false;
         }
     }
 }
