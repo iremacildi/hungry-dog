5cd5e2ea2f124af1bd7811f4c5dbc31a8b101fb9
iremacildi <iremacildi@gmail.com>
2022-06-05 15:53:07 +0300

    show hunger level as text

--- Assets/Scripts/CollectingFood.cs
+++ Assets/Scripts/CollectingFood.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CollectingFood : MonoBehaviour
 {
     public int hungerLevel;
+    private bool _isFull;
 
     void Start()
     {
@@ -13,24 +14,29 @@ public class CollectingFood : MonoBehaviour
 
     public void OnTriggerEnter(Collider Col)
     {
-        if(Col.gameObject.tag == "Carrot")
+        if(Col.gameObject.tag == "Carrot" && !_isFull)
         {
             hungerLevel = hungerLevel + 2;
             Destroy(Col.gameObject.transform.parent.gameObject);
             GetComponentInParent<Hunger>().ModifyHunger(2);
         }
-        if(Col.gameObject.tag == "Pea")
+        if(Col.gameObject.tag == "Pea" && !_isFull)
         {
             hungerLevel = hungerLevel + 2;
             Destroy(Col.gameObject.transform.parent.gameObject);
             GetComponentInParent<Hunger>().ModifyHunger(2);
         }
-        else if(Col.gameObject.tag == "Meat")
+        else if(Col.gameObject.tag == "Meat" && !_isFull)
         {
             hungerLevel = hungerLevel + 8;
             Destroy(Col.gameObject.transform.parent.gameObject);
             GetComponentInParent<Hunger>().ModifyHunger(8);
         }
+    }
+
+    public void SetIsFull(bool isFull)
+    {
+        _isFull = isFull;
     }
 
     void Update()
--- Assets/Scripts/Hunger.cs
+++ Assets/Scripts/Hunger.cs
@@ -1,40 +1,94 @@
 using System;
 using UnityEngine;
+using TMPro;
 
 public class Hunger : MonoBehaviour
 {
     [SerializeField]
-    private int maxHunger = 100;
-    private int currentHunger;
+    private int maxHungerValue = 100;
+    [SerializeField]
+    private TextMeshProUGUI currentHunger;
+    [SerializeField]
+    private TextMeshProUGUI seperator;
+    [SerializeField]
+    private TextMeshProUGUI maxHunger;
+    private int currentHungerValue;
     private GameObject chestOpen;
     private GameObject chestClose;
     public event Action<float> OnHungerPctChanged = delegate { };
+    private float flashHunger;
+    private float flashDuration = 1f;
+    private bool isFull;
 
     private void OnEnable() 
     {
         chestOpen = GameObject.Find("Chest_Open");
+        chestOpen.SetActive(false);
         chestClose = GameObject.Find("Chest_Close");
         chestClose.SetActive(true);
-        currentHunger = 0;
+        currentHungerValue = 0;
+        maxHunger.text = maxHungerValue.ToString();
     }
 
     public void ModifyHunger(int amount)
     {
-        currentHunger += amount;
+        if(!isFull)
+        {
+            currentHungerValue += amount;
 
-        float currentHungerPct = (float)currentHunger / (float)maxHunger;
-        OnHungerPctChanged(currentHungerPct);
+            float currentHungerPct = (float)currentHungerValue / (float)maxHungerValue;
+            OnHungerPctChanged(currentHungerPct);
+            UpdateHungerDisplay();
+        }        
 
-        if(currentHunger >= 100){
+        if(currentHungerValue >= maxHungerValue){
+            currentHungerValue = maxHungerValue;
+            isFull = true;
+            GetComponentInParent<CollectingFood>().SetIsFull(true);
             chestOpen.SetActive(true);
             chestClose.SetActive(false);
         }
+    }
+
+    private void UpdateHungerDisplay()
+    {
+        currentHunger.text = currentHungerValue.ToString();
+    }
+
+    private void Flash()
+    {
+        if(currentHungerValue != maxHungerValue){
+            currentHungerValue = maxHungerValue;
+            UpdateHungerDisplay();
+        }
+
+        if(flashHunger <= 0){
+            flashHunger = flashDuration;
+        }
+        else if(flashHunger >= flashDuration / 2){
+            flashHunger -= Time.deltaTime;
+            SetTextDisplay(false);
+        }
+        else{
+            flashHunger -= Time.deltaTime;
+            SetTextDisplay(true);
+        }
+    }
+
+    private void SetTextDisplay(bool enabled){
+        currentHunger.enabled = enabled;
+        seperator.enabled = enabled;
+        maxHunger.enabled = enabled;
     }
 
     private void Update()
     {
         if(Input.GetKeyDown(KeyCode.K)){
             ModifyHunger(+10);
+        }
+
+        if(currentHungerValue >= maxHungerValue){
+            Flash();
         }
     }
 }
